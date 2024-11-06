ac3d8d0f1998c3c6a80a8889e60d1f0145977776
Satwinder04 <satwindersalan@gmail.com>
2024-11-06 19:49:51 +0530

    Nov62024

--- day1.py
+++ day1.py
@@ -13,7 +13,7 @@
 # b=2.4
 # c="wellcome"
 # print(a,b,c)
-
+#--------------------------------------------------------------------------
 # example1
 # a=100
 # b=100
@@ -21,7 +21,7 @@
 # a=b=c = 100
 # print(a,b,c)
 
-
+#--------------------------------------------------------------------------
 # Swapping
 # x=10
 # y=20
@@ -29,7 +29,7 @@
 # print("before Swapping","x = ",x, "y = ",y)
 # x,y=y,x
 # print("After Swapping","x = ",x, "y = ",y)
-
+#--------------------------------------------------------------------------
 # deleting var
 # a=10
 # b=20
@@ -40,9 +40,9 @@
 # del a
 # print(a)
 # print(b)
-
+#--------------------------------------------------------------------------
 # oPratores
-
+# Assignment oprators
 # a=6
 # b=2
 # print(a+b)
@@ -53,6 +53,12 @@
 # print(a**b) # power
 # print(a*b)
 
+# a=43
+# a+=3 # it will increment the value by 3 and assign to the "a" variable
+# a-=3 # it will increment the value by 3 and assign to the "a" variable
+# print(a)
+
+#--------------------------------------------------------------------------
 # Simple interst
 # SI = P × R × T / 100
 
@@ -72,7 +78,7 @@
 # radius = float( input("enter radius = "))
 # ar = math.pi * radius**2
 # print(ar)
-
+#--------------------------------------------------------------------------
 # relational opreators
 # a=5
 # b=6
@@ -82,14 +88,14 @@
 # print(a!=b)
 # print(a<=b)
 # print(a>=b)
-
+#--------------------------------------------------------------------------
 #Logical Opreatoer
 # a= True
 # b=False
 # print(a and b)
 # print(a or b)
 # print(not b)
-
+#--------------------------------------------------------------------------
 # Lists
 # x=[1,2,3,4,5]
 # y=x #it uses reference
@@ -98,7 +104,7 @@
 # print(x is z)
 # print(x is not y)
 # print(x is not z)
-
+#--------------------------------------------------------------------------
 #Diffrent ways to printting
 
 # name,age,sal = "aman",25,100000
@@ -115,9 +121,32 @@
 # b= int(input("Enter the value of b : "))
 # c = a+b
 # print("Sum of a and b is :",c)
+#
+# a= int(input("Enter the value of a : "))
+# b= int(input("Enter the value of b : "))
+# c = a%b
+# print("remander of a and b is :",c)
 
-a= int(input("Enter the value of a : "))
-b= int(input("Enter the value of b : "))
-c = a%b
-# print("Sum of a and b is :",c)
-print(f"Sum of a and b is : {c}")
+#WAP no. greater then b or not
+#
+# a= int(input("enter value of a "))
+# b= int(input("enter value of b "))
+#
+# # c=int(a>b)
+# c=str(a>b)
+# # c=a>b
+# print("A is greater then b" ,type(c),c)
+
+#WAP av no. of user
+# a= int(input("first No."))
+# b= int(input("2nd No."))
+# c= int(input("3rd No."))
+# av= (a+b+c)/3
+
+# a= input("first No.")
+# b= input("2nd No.")
+# c= input("3rd No.")
+#
+# av =(int(a)+int(b)+int(c))/3
+# print(av)
+
